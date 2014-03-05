73204709792b4c3e24b5e9e59c1503a8a54a1b07
Steven <mrpontius@gmail.com>
2014-03-05 10:45:18 +0000

    update from lab, weird 404 errors for directories that dont exist. NOT FILES

--- server.c
+++ server.c
@@ -17,7 +17,7 @@
 #define CONNMAX 1000
 #define BYTES 1024
 #define ERROR404 "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: 217\r\nConnection: close\r\n\r\n<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\"><html>\r\n<head>\r\n<title> 404 Not Found </title>\r\n</head>\r\n<body>\r\n<p> 404 Not Found </p>\r\n</body>\r\n</html>\r\n"
-#define ERROR400 "HTTP/1.0 400 Bad Request\r\nContent-Type: text/html\r\nContent-Length: 217\r\nConnection: close\r\n\r\n<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\"><html>\r\n<head>\r\n<title> 404 Not Found </title>\r\n</head>\r\n<body>\r\n<p> 404 Not Found </p>\r\n</body>\r\n</html>\r\n"
+#define ERROR400 "HTTP/1.1 400 Bad Request\r\nContent-Type: text/html\r\nContent-Length: 217\r\nConnection: close\r\n\r\n<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\"><html>\r\n<head>\r\n<title> 400 Not Found </title>\r\n</head>\r\n<body>\r\n<p> 400 Not Found </p>\r\n</body>\r\n</html>\r\n"
 
 
 #include <unistd.h>
@@ -151,10 +151,10 @@ int main(int argc, char* argv[])
         else
         {
 
-                printf("%d request number", slot);
-               pthread_t thread;
-                pthread_create(&thread, NULL, pthread_run, (void*)&slot);
-                printf("thread created\n");
+            printf("%d request number", slot);
+            pthread_t thread;
+            pthread_create(&thread, NULL, pthread_run, (void*)&slot);
+            printf("thread created\n");
                // respond(slot, ROOT, clients); //client number is passed into respond function to deal with requests
                 //exit(0);
             
@@ -163,33 +163,12 @@ int main(int argc, char* argv[])
        // while (clients[slot]!=-1) slot = (slot+1)%CONNMAX; //incriment the counter for the number of clients
     }
 
+    //Closing SOCKET
+    //shutdown (clients[slot], SHUT_RDWR);         //All further send and recieve operations are DISABLED...
+    //close(clients[slot]);
+    //clients[slot]=-1;
+
     return 0;
-}
-
-//start server
-int startServer(char *port) //alot of this is copied from my ns3 lab 1
-{
-    int listenfd, portint;
-
-	sscanf(port, "%d", &portint);
-	
-	int type = AF_INET6;
-
-	listenfd = socket(type, SOCK_STREAM, 0);
-	if(listenfd == -1) perror("error creting socket");
-
-	struct sockaddr_in6 address;
-	
-	address.sin6_addr = in6addr_any;
-
-	address.sin6_family = type;
-	address.sin6_port = htons(portint);
-	if (bind(listenfd, (struct sockaddr *) &address, sizeof(address)) == -1) perror("problem binding");
-
-	if (listen(listenfd, 1000000) == -1) perror("problem listening");
-
-    return listenfd;
-    
 }
 
 void response400(int fd){
@@ -209,17 +188,19 @@ void response200(int client, char* reqline[], char path[], char* ROOT){
 
     char data_to_send[BYTES];
 
-    char * copy = malloc(strlen(reqline[1]) + 1); 
+    char * copy = calloc(1024,strlen(reqline[1]) + 1); 
+    if (copy == NULL) perror("MALLOC FAILED RESPONSE200 COPY\n");
     strcpy(copy, reqline[1]);
 
-    char* contentlength = malloc(1024);
+    char* contentlength = calloc(1024,1024);
+    if (contentlength == NULL) perror("MALLOC FAILED CONTENTLENGTH RESPONSE 200\n");
 
     //char* bytesize = malloc(sizeof(int));
 
     char* type = findType(copy); //find content type
     if (type == NULL){
         write(client, ERROR404, strlen(ERROR404));
-        close(client);
+        //close(client);
     } 
     int counter = 0;
     while (reqline[counter] = strtok (NULL, "\r\n")) counter++;
@@ -248,58 +229,19 @@ void response200(int client, char* reqline[], char path[], char* ROOT){
         free(contentlength);
     }
 
-    else write(client, ERROR404, strlen(ERROR404)); //FILE NOT FOUND, 404 ERROR
+    else{ write(client, ERROR404, strlen(ERROR404)); //FILE NOT FOUND, 404 ERROR
+    	printf("IS THIS HAPPENING\n");}
 
 }
 
-//client connection
-void respond(int n, char* ROOT, int clients[]){
-	char mesg[99999], *reqline[128],  path[99999];
-	int rcvd;
 
-    memset( (void*)mesg, (int)'\0', 99999 );
-
-    rcvd=recv(clients[n], mesg, 99999, 0);
-
-    if (rcvd<0)    // receive error
-       	fprintf(stderr,("recv() error\n")); //no request
-    else if (rcvd==0)    // receive socket closed
-       	fprintf(stderr,"Client disconnected upexpectedly.\n");
-    else{    // message received{
-       // printf("%s", mesg);
-        reqline[0] = strtok (mesg, " \t\n"); //part of the request that should be "GET"
-
-        if ( strncmp(reqline[0], "GET\0", 4)!=0 ){
-        	//printf("\n\nreqline0 %s", reqline[0]);
-        	response400(clients[n]);
-        }
-
-        else if ( strncmp(reqline[0], "GET\0", 4)==0 ){
-            reqline[1] = strtok (NULL, " \t"); //the file being requested
-
-            reqline[2] = strtok (NULL, " \t\n"); //the part of the request that should say "HTTP/1.1" or "HTTP/1.0"
-            if ( strncmp( reqline[2], "HTTP/1.0", 8)!=0 && strncmp( reqline[2], "HTTP/1.1", 8)!=0 ){
-            	//printf("\n\nreqline2 %s", reqline[2]);
-				response400(clients[n]);
-            }
-            else{
-            	if ( strncmp(reqline[1], "/\0", 2)==0 ) reqline[1] = "/index.html"; //because if no file is specified, index.html will be opened by default
-            	response200(clients[n], reqline, path, ROOT);                
-            	}
-        	}
-    	}
-
-    //Closing SOCKET
-    shutdown (clients[n], SHUT_RDWR);         //All further send and recieve operations are DISABLED...
-    close(clients[n]);
-    clients[n]=-1;
-}
 
 void findHost(char *reqline[], int fd){ //identifies the host header in the request, this is needed since im not assuming its in a static position
 	int c = 0;
 	char* h;
-	char* host;
+	char* requesthost;
 	char* currenthost = malloc(1024);
+	if (currenthost == NULL) perror("MALLOC FAILED FINDHOST\n");
 	
 	char* domain = ".dcs.gla.ac.uk";
 	char* local = "localhost";
@@ -317,11 +259,18 @@ void findHost(char *reqline[], int fd){ //identifies the host header in the requ
 	if ( strncmp(reqline[c], "Host: ", 6)==0 ){ //finds the host header
 
 		h = strdup(reqline[c]+6);	
-		host = strtok(h,":");
-		hostdomain = strdup(host);
-		strncat(hostdomain, domain, 64);
+		requesthost = strtok(h,":");
+		hostdomain = strdup(requesthost);
+		//strncat(hostdomain, domain, 64);
 
-		if (strcmp(currenthost, local) != 0 && strcmp(currenthost, host) != 0 && strcmp(currenthost, hostdomain) !=0 && strcmp(host, "localhost") != 0) response400(fd); //need to remove last strcmp, figure out why it wont send http request when using the computer name
+		printf("%s ROFL LOOK HERE ERERERERERERE\n", hostdomain);
+
+		printf("%s ROFL LOOK HERE ERERERERERERE\n", requesthost);
+		printf("%s ROFL LOOK HERE ERERERERERERE\n", currenthost);
+
+		//if (strcmp(currenthost, local) != 0 && strcmp(currenthost, host) != 0 && strcmp(currenthost, hostdomain) !=0 && strcmp(host, "localhost") != 0) response400(fd); //need to remove last strcmp, figure out why it wont send http request when using the computer name
+		if (strcmp(requesthost, currenthost) != 0 && strcmp(requesthost, "localhost") != 0) response400(fd);
+
 		c = 50; //makes it exit the loop
         free(h);
         //free(host);
